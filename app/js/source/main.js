0730c2499a8e2fe42fab901ca27716e8ac8348a7
Richmond Watkins <richmondwatkins@mac.com>
2014-04-20 11:23:35 -0500

    completed game

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -6,38 +6,100 @@
   var player = 1;
 
   function initialize(){
-    $('#board').on('click', 'td.empty', select);
+    $('#board').on('click', 'td.table', select);
+    $('#reset').click(reset);
+  }
+
+  function reset(){
+    var newGame = $('tbody > tr > td');
+    $($(newGame)).removeClass('x');
+    $($(newGame)).removeClass('o');
+    $($(newGame)).addClass('table');
   }
 
   function select(){
 
     if(player===1){
-      $(this).append('x');
-      $(this).addClass('x');
-      $(this).removeClass('empty');
-    } else {
-      $(this).append('o');
-      $(this).addClass('o');
-      $(this).removeClass('empty');
-    }
-    // gameArray.push(player);
-    checkWin();
-    player = player * -1;
 
+      // $(this).text('x');
+      $(this).addClass('x');
+      $(this).removeClass('table');
+
+    } else {
+      // $(this).text('o');
+      $(this).addClass('o');
+      $(this).removeClass('table');
+    }
+      player = player * -1;
+      checkWin();
   }
 
   function checkWin(){
+    var tdArray1 = $('tbody > tr:nth-child(1) > td');
+    var tdArray2 = $('tbody > tr:nth-child(2) > td');
+    var tdArray3 = $('tbody > tr:nth-child(3) > td');
 
-    if($('td.x').length >= 3 || $('td.y').length >= 3) {
-      alert('you win');
-    }
+
+
+
+
+      if($(tdArray1[0]).hasClass('x') && $(tdArray1[1]).hasClass('x') && $(tdArray1[2]).hasClass('x')){
+        alert('Facebook Wins!');
+
+      }
+      if($(tdArray2[0]).hasClass('x') && $(tdArray2[1]).hasClass('x') && $(tdArray2[2]).hasClass('x')){
+        alert('Facebook Wins!');
   }
+      if($(tdArray3[0]).hasClass('x') && $(tdArray3[1]).hasClass('x') && $(tdArray3[2]).hasClass('x')){
+        alert('Facebook Wins!');
 
-  // function whoseTurn(){
-  //
-  //
-  // }
+  }
+      if($(tdArray1[0]).hasClass('x') && $(tdArray2[0]).hasClass('x') && $(tdArray3[0]).hasClass('x')){
+        alert('Facebook Wins!');
 
-  //// [0,1.0,1....]
+  }
+      if($(tdArray1[1]).hasClass('x') && $(tdArray2[1]).hasClass('x') && $(tdArray3[1]).hasClass('x')){
+        alert('Facebook Wins!');
+}
+      if($(tdArray1[2]).hasClass('x') && $(tdArray2[2]).hasClass('x') && $(tdArray3[2]).hasClass('x')){
+        alert('Facebook Wins!');
+}
+      if($(tdArray1[0]).hasClass('x') && $(tdArray2[1]).hasClass('x') && $(tdArray3[2]).hasClass('x')){
+        alert('Facebook Wins!');
+}
+
+      if($(tdArray1[2]).hasClass('x') && $(tdArray2[1]).hasClass('x') && $(tdArray3[0]).hasClass('x')){
+        alert('Facebook Wins!');
+}
+      //============== start of the 'o's ============
+
+      if($(tdArray1[0]).hasClass('o') && $(tdArray1[1]).hasClass('o') && $(tdArray1[2]).hasClass('o')){
+
+        alert('Twitter Wins!');
+      }
+      if($(tdArray2[0]).hasClass('o') && $(tdArray2[1]).hasClass('o') && $(tdArray2[2]).hasClass('o')){
+        alert('Twitter Wins!');
+  }
+      if($(tdArray3[0]).hasClass('o') && $(tdArray3[1]).hasClass('o') && $(tdArray3[2]).hasClass('o')){
+        alert('Twitter Wins!');
+  }
+      if($(tdArray1[0]).hasClass('o') && $(tdArray2[0]).hasClass('o') && $(tdArray3[0]).hasClass('o')){
+        alert('Twitter Wins!');
+
+  }
+      if($(tdArray1[1]).hasClass('o') && $(tdArray2[1]).hasClass('o') && $(tdArray3[1]).hasClass('o')){
+        alert('Twitter Wins!');
+}
+      if($(tdArray1[2]).hasClass('o') && $(tdArray2[2]).hasClass('o') && $(tdArray3[2]).hasClass('o')){
+        alert('Twitter Wins!');
+}
+      if($(tdArray1[0]).hasClass('o') && $(tdArray2[1]).hasClass('o') && $(tdArray3[2]).hasClass('o')){
+        alert('Twitter Wins!');
+}
+
+      if($(tdArray1[2]).hasClass('o') && $(tdArray2[1]).hasClass('o') && $(tdArray3[0]).hasClass('o')){
+        alert('Twitter Wins!');
+}
+}
 
 })();
